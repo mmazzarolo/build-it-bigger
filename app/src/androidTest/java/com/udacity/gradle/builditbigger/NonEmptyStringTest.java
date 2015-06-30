c3136a6ba5bf262e04f6314514261ccbf298d41d
Matteo <mazzarolomatteo@gmail.com>
2015-06-30 12:57:39 +0200

    Step 4: Add Functional Tests

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/NonEmptyStringTest.java
@@ -0,0 +1,30 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+/**
+ * Created by Matteo on 30/06/2015.
+ */
+public class NonEmptyStringTest extends AndroidTestCase {
+
+    private static final String LOG_TAG = "NonEmptyStringTest";
+
+    @SuppressWarnings("unchecked")
+    public void runTest() {
+
+        // Testing that Async task successfully retrieves a non-empty string
+        // You can test this from androidTest -> Run 'All Tests'
+        String result = null;
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(getContext());
+        endpointsAsyncTask.execute();
+        try {
+            result = endpointsAsyncTask.get();
+            Log.d(LOG_TAG, "Retrieved a non-empty string successfully: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(result);
+    }
+
+}
