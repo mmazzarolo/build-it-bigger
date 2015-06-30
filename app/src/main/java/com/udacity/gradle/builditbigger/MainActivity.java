8f93c04bbf6bdf4eff5908b4be1c2e077a4e55d2
Matteo <mazzarolomatteo@gmail.com>
2015-06-30 14:35:21 +0200

    Step 5: Add a Paid Flavor

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.JokeTelling;
-import com.example.matteo.myandroidlibrary.JokeDisplayActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view){
+    public void tellJoke(View view) {
         new EndpointsAsyncTask(this).execute();
     }
 }
